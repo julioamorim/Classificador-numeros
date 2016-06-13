59aae9a5cdeebe5aaf59b052e6f64635f61d96fa
rafael--- <rafaelhr.ribeiro@gmail.com>
2016-06-13 16:11:47 -0300

    Versão funcionando

--- teste.cpp
+++ teste.cpp
@@ -3,8 +3,18 @@
 #include <cstdio>
 #include <climits>
 #include <fann.h>
-
+#include <iomanip>
 using namespace std;
+
+
+float bin_to_float(float * vetor, int tamanho){
+    int max = 0;
+    for(int i = 1; i < tamanho; i++){
+        if(vetor[i] > vetor[max])
+            max = i;
+    }
+    return float(max);
+}
 
 int main(){
     struct fann *ann = fann_create_from_file("number_classify.net");
@@ -14,18 +24,27 @@ int main(){
     int linhas, n_entradas, n_saidas;
     arq >> linhas >> n_entradas >> n_saidas;
 
-    for(int k = 0; k < 10; k++){
+    int erros =0, acertos = 0;
+    for(int k = 0; k < linhas; k++){
         for(int i = 0; i < n_entradas; i++)
             arq >> entradas[i];
         for(int i = 0; i < n_saidas; i++)
             arq >> saida_real[i];
 
         float * saidas = fann_run(ann, entradas);
-        cout << "Posicao\tSaída:\tValor real:\n";
+        /*cout << "Posicao\tSaída:\tValor real:\n";
         for(int i = 0; i < n_saidas; i++){
             cout << i << '\t' << saidas[i] << '\t' << saida_real[i] << '\n';
-        }
+        }*/
+        float fsaida=bin_to_float(saidas,n_saidas),  freal = bin_to_float(saida_real, n_saidas);
+        cout << "Saída: " << fsaida << "\tValor real: " << freal << '\n';
+        if(fsaida == freal)
+            acertos++;
+        else
+            erros++;
     }
+    cout << "Erros: " << erros << "/" << linhas << '\n';
+    cout << "Taxa de acerto: " <<  float(acertos)/linhas*100 << "% \n";
     fann_destroy(ann);
     return 0;
 }
--- treinamento.cpp
+++ treinamento.cpp
@@ -5,23 +5,26 @@ int main()
     const unsigned int entradas = 64;
     const unsigned int saidas = 10;
     const unsigned int camadas = 3;
-    const unsigned int neuronios_escondidos = 30;
-    const float erro_maximo = 0.000001f;
-    const unsigned int max_epocas = 5000;
-    const unsigned int epocas_entre_testes = 100;
+    const unsigned int neuronios_escondidos = 55;
+    const float erro_maximo = 0.0f;
+    const unsigned int max_epocas = 50000;
+    const unsigned int epocas_entre_impressoes = 100;
 
-    struct fann *ann = fann_create_standard(camadas, entradas,
+    struct fann *rede = fann_create_standard(camadas, entradas,
             neuronios_escondidos,saidas);
 
-    fann_set_activation_function_hidden(ann, FANN_SIGMOID);
-    fann_set_activation_function_output(ann, FANN_SIGMOID);
+    fann_set_activation_function_hidden(rede, FANN_SIGMOID);
+    fann_set_activation_function_output(rede, FANN_SIGMOID);
 
-    fann_train_on_file(ann, "input/treinamento/optdigits2.tra", max_epocas,
-            epocas_entre_testes, erro_maximo);
+    fann_set_training_algorithm(rede, FANN_TRAIN_INCREMENTAL);
+    fann_set_learning_rate(rede, 0.05f);
 
-    fann_save(ann, "number_classify.net");
+    fann_train_on_file(rede, "input/treinamento/optdigits2.tra", max_epocas,
+            epocas_entre_impressoes, erro_maximo);
 
-    fann_destroy(ann);
+    fann_save(rede, "number_classify.net");
+
+    fann_destroy(rede);
 
     return 0;
 }
