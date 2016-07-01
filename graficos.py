d8f10cc038979f46f99601e632629c3c5996316a
rafael--- <rafaelhr.ribeiro@gmail.com>
2016-07-01 09:50:11 -0300

    Gráficos com python

---
+++ graficos.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import plotly
+from plotly.graph_objs import Scatter, Layout
+from sys import argv, exit
+
+fname = ""
+try:
+    fname = argv[1]
+except:
+    print("Digite o nome do arquivo de dados")
+    exit(0)
+
+f = open(fname)
+
+epocas = []
+mse = []
+
+for line in f:
+    s = line[:-1].split(',')
+    epocas.append(int(s[0]))
+    mse.append(float(s[1]))
+
+plotly.offline.plot({"data":[Scatter(x=epocas, y=mse)],"layout":Layout(title="Aprendizagem")})
